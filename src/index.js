d81ecf643d40a07ac3f25ea7932a96b94b76cb16
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-07-30 19:12:35 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './css/styles.css';
 import './js/app.js';
+import '../node_modules/simplelightbox/src/simple-lightbox.scss';
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 import cardItemsTpl from '../templates/cardItem';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 
-import CardsApiService from './fetchCards';
-import LoadMoreBtn from './loadMoreBtn';
+import CardsApiService from './fetchCards.js';
+import LoadMoreBtn from './loadMoreBtn.js';
 
 const cardsApiService = new CardsApiService();
 const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
   hidden: true,
 });
+const lightbox = new SimpleLightbox('.photo-card a');
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
@@ -16,31 +19,55 @@ const refs = {
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
-loadMoreBtn.refs.button.addEventListener('click', fetchCards);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 async function onSearch(e) {
   e.preventDefault();
 
-  cardsApiService.query = e.currentTarget.elements.searchQuery.value;
+  cardsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
 
   if (cardsApiService.query === '') {
     return;
   }
 
-  loadMoreBtn.show();
   cardsApiService.resetPage();
   clearCardsGallery();
-  fetchCards();
+  try {
+    const getCards = await cardsApiService.getCards();
+    if (getCards.totalHits === 0) {
+      loadMoreBtn.hide();
+      showMessage('empty');
+    }
+    if (getCards.totalHits > 0) {
+      loadMoreBtn.show();
+      renderCards(getCards.hits);
+      showMessage(getCards.totalHits);
+      loadMoreBtn.enable();
+      lightbox.refresh();
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-async function fetchCards() {
+async function onLoadMore() {
   loadMoreBtn.disable();
+
   try {
     const getCards = await cardsApiService.getCards();
     renderCards(getCards.hits);
     loadMoreBtn.enable();
+    lightbox.refresh();
+
+    const { height: cardHeight } = refs.cardList.firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 1.5,
+      behavior: 'smooth',
+    });
   } catch (error) {
-    console.log(error);
+    loadMoreBtn.hide();
+    showMessage('end');
   }
 }
 
@@ -51,4 +78,16 @@ function renderCards(card) {
 
 function clearCardsGallery() {
   refs.cardList.innerHTML = '';
+}
+
+function showMessage(message) {
+  if (message === 'end') {
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+  } else if (message === 'empty') {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  } else if (message === Number(message)) {
+    Notiflix.Notify.info(`Hooray! We found ${message} images.`);
+  }
 }
